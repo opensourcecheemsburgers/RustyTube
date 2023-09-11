413370ff9d47ce66b4f7b9cdae3cbb4dadd8e49d
Stephen Power <simpilldev@gmail.com>
2023-09-11 09:53:00 +0300

    Add Option<String> on some variants of ColorInfo enum

--- shared/invidious/src/formats/items/color_info.rs
+++ shared/invidious/src/formats/items/color_info.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ColorInfo {
-	pub primaries: String,
+	pub primaries: Option<String>,
 	pub transfer_characteristics: String,
-	pub matrix_coefficients: String,
+	pub matrix_coefficients: Option<String>,
 }
