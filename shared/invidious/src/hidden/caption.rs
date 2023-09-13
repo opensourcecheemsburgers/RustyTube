513215b41d98afbf2f638fae9bdb91c02f272f34
Stephen Power <simpilldev@gmail.com>
2023-09-13 14:08:00 +0300

    Add url fetch for invidious captions

--- shared/invidious/src/hidden/caption.rs
+++ shared/invidious/src/hidden/caption.rs
@@ -9,6 +9,12 @@ pub struct Caption {
     pub url: String,
 }
 
+impl Caption {
+    pub fn url(&self, server: &str) -> String {
+        format!("{}{}", server, self.url)
+    }
+}
+
 impl PartialEq for Caption {
     fn eq(&self, other: &Self) -> bool {
         *&self.url.eq_ignore_ascii_case(&other.url)
