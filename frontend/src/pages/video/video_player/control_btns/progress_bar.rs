2850556e01ba08f5c7552c99f80ffad48bff9f74
Stephen Power <simpilldev@gmail.com>
2023-11-08 21:37:00 +0300

    Add video time tip for progress bar

--- frontend/src/pages/video/video_player/control_btns/progress_bar.rs
+++ frontend/src/pages/video/video_player/control_btns/progress_bar.rs
@@ -2,7 +2,10 @@ use leptos::*;
 use wasm_bindgen::JsCast;
 use web_sys::{DragEvent, HtmlProgressElement, MouseEvent};
 
-use crate::contexts::{PlayerState, PlayerStyle};
+use crate::{
+    components::{Tooltip, TooltipPosition},
+    contexts::{PlayerState, PlayerStyle},
+};
 
 #[component]
 pub fn ProgressBar() -> impl IntoView {
@@ -13,91 +16,50 @@ pub fn ProgressBar() -> impl IntoView {
         state.seek(seek_pos(mouse_event));
     };
     let on_drag = move |drag_event: DragEvent| {
-        state.seek(seek_drag_pos(drag_event));
+        state.seek(seek_pos(drag_event));
+    };
+
+    let tip_styles = RwSignal::new("bottom: 48px; left: 0px;".to_string());
+    let tip_classes = RwSignal::new("hidden absolute p-2 rounded-lg z-100 bg-primary text-primary-content h-fit w-fit".to_string());
+    let tip_time = RwSignal::new("0:00".to_string());
+
+    let open_tip = move |mouse_event: MouseEvent| {
+        tip_time.set(get_seek_pos_as_time_str(mouse_event.clone()));
+        let styles = format!("bottom: 48px; left: {}px;", mouse_event.offset_x());
+        tip_styles.set(styles);
+        tip_classes.set("absolute p-2 rounded-lg z-100 bg-primary text-primary-content h-fit w-fit".to_string());
+        
+    };
+    let close_tip = move |mouse_event| {
+        tip_classes.set("hidden absolute p-2 rounded-lg z-100 bg-primary text-primary-content h-fit w-fit".to_string());
     };
 
     view! {
+        <div style=tip_styles class=tip_classes>{tip_time}</div>
         <progress
+            on:mouseover=open_tip
+            on:mousemove=open_tip
+            on:mouseout=close_tip
             on:click=on_click
             on:dragend=on_drag
             data-controlsvisible=style.controls_visible
             max=state.duration.read_only()
             value=state.current_time.read_only()
-            class=PROGRESS_BAR
+            class="video-progress-bar"
         ></progress>
     }
 }
 
-fn seek_pos(mouse_event: MouseEvent) -> f64 {
-    let progress: HtmlProgressElement = mouse_event.target().unwrap().dyn_into().unwrap();
-    let x = mouse_event.offset_x() as f64;
+fn get_seek_pos_as_time_str<E>(event: E) -> String where E: AsRef<MouseEvent>  {
+    let seek_pos = seek_pos(event);
+    utils::unix_to_hours_secs_mins(seek_pos)
+}
+
+fn seek_pos<E>(event: E) -> f64 where E: AsRef<MouseEvent> {
+    let progress: HtmlProgressElement = event.as_ref().target().unwrap().dyn_into().unwrap();
+    let x = event.as_ref().offset_x() as f64;
     let offset_width = progress.offset_width() as f64;
     let max = progress.max();
 
     x * max / offset_width
 }
-
-fn seek_drag_pos(drag_event: DragEvent) -> f64 {
-    let progress: HtmlProgressElement = drag_event.target().unwrap().dyn_into().unwrap();
-    let x = drag_event.offset_x() as f64;
-    let offset_width = progress.offset_width() as f64;
-    let max = progress.max();
-
-    x * max / offset_width
-}
-
-const PROGRESS_BAR: &'static str = "
-    peer/progress z-10
-
-    progress progress-primary bg-neutral opacity-100 h-1 w-full cursor-pointer
-    
-    transition-all ease-in duration-300
-
-    hover:ease-in 
-    hover:duration-300
-    hover:opacity-100
-    hover:mt-0
-    hover:h-1
-    hover:scale-y-[2.5]
-
-    active:ease-in 
-    active:duration-300
-    active-focus:opacity-100
-    active:mt-0
-    active:rounded-none
-    active:h-1
-    active:scale-y-[2.5]
-
-    focus:ease-in 
-    focus:duration-300
-    focus-focus:opacity-100
-    focus:mt-0
-    focus:rounded-none
-    focus:h-1
-    focus:scale-y-[2.5]
-
-    peer-active/controls:ease-in 
-    peer-active/controls:duration-300
-    peer-active/controls:opacity-100
-    peer-active/controls:mt-0
-    peer-active/controls:rounded-none
-    peer-active/controls:h-1
-    peer-active/controls:scale-y-[2.5]
-
-    peer-hover/controls:ease-in 
-    peer-hover/controls:duration-300
-    peer-hover/controls:opacity-100
-    peer-hover/controls:mt-0
-    peer-hover/controls:rounded-none
-    peer-hover/controls:h-1
-    peer-hover/controls:scale-y-[2.5]
-
-    peer-focus/controls:ease-in 
-    peer-focus/controls:duration-300
-    peer-focus/controls:opacity-100
-    peer-focus/controls:mt-0
-    peer-focus/controls:rounded-none
-    peer-focus/controls:h-1
-    peer-focus/controls:scale-y-[2.5]
-";
-
