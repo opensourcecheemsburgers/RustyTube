438601b7f6374f4f43b51d8f929812b57476c941
Stephen Power <simpilldev@gmail.com>
2023-11-19 19:44:00 +0300

    Update stuff

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
     plugins: [
         require('daisyui'),
         require('@tailwindcss/typography'),
-        require("tailwindcss-animate")
     ],
     theme: {
         fontFamily: {
